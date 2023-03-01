5c9ee55896f248f3b3eb0fcbe726ab0e8031fefd
LuesbelinMejia <luesbelinmejia@yahoo.com>
2023-02-28 23:31:59 -0600

    commit

--- Views/Template/nav_admin.php
+++ Views/Template/nav_admin.php
@@ -28,7 +28,7 @@
         </ul>
       </li>
         <li><a class="app-menu__item" href="<?= base_url(); ?>/compras"><i class="app-menu__icon fa fa-shopping-bag"></i><span class="app-menu__label">Compras</span></a></li>
-        <li><a class="app-menu__item" href="<?= base_url(); ?>/productos"><i class="app-menu__icon 	fa fa-folder-open"></i><span class="app-menu__label">Inventario</span></a></li>
+        <li><a class="app-menu__item" href="<?= base_url(); ?>/productos"><i class="app-menu__icon fa fa-file-code-o"></i><span class="app-menu__label">Inventario</span></a></li>
 
         <li>
             <a class="app-menu__item" href="<?= base_url(); ?>/logout">
